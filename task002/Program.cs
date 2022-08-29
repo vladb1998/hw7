43754cbe926835e2f3837ce63da4162d66585bdb
vlad <vladbelyvskij@gmail.com>
2022-08-29 22:08:17 +0300

    Задача 2

---
+++ task002/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Console.WriteLine();
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,100));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ Console.WriteLine();
+ Console.WriteLine("Введите координаты");
+ System.Console.Write("Строка: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ System.Console.Write("Столбец: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ int  c = array[a,b];
+ Console.WriteLine(c);
+ }
