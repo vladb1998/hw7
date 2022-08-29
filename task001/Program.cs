27147f553e5e1ccf469afa9dd8763bd7ecaaa844
vlad <vladbelyvskij@gmail.com>
2022-08-29 22:07:36 +0300

    Задача 1

---
+++ task001/Program.cs
@@ -0,0 +1,10 @@
+﻿double[,] arr = new double [3 , 4];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+   for (int j = 0; j < arr.GetLength(1); j++)
+   {
+    arr [i,j] = new Random().NextDouble();
+    System.Console.Write($"{Math.Round (arr[i,j] , 2)} ; ");
+   }
+   System.Console.WriteLine();
+} 
